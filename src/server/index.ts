84922298c012504e7307a6d6a870f6f239d44a80
juancodev <montillasanchezjuancarlos@gmail.com>
2023-04-26 23:54:26 -0400

    [⚡ ADD] html on the server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,11 @@
 import express, { Express, Request, Response } from "express";
 import { config } from "./config/";
+import { template } from './render/template';
 
 const app: Express = express();
 
 app.get('/', (request: Request, response: Response) => {
-  response.send(`<h1>Hello world from ${request.url}</h1>`);
+  response.send(template(`<h1>Hello world from: ${request.url}</h1>`));
 });
 
 app.get('*', (request: Request, response: Response) => {
