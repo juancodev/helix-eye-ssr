c7069a9aa21414f74d03642ed2801969ed4126d1
juancodev <montillasanchezjuancarlos@gmail.com>
2023-05-03 12:09:37 -0400

    [⚡ ADD] inyecting the response from my server to the client

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import { config } from "./config/";
 import { render } from "./render";
+import axios from "axios";
 
 const app: Express = express();
 
@@ -8,6 +9,18 @@ app.use(express.static('dist'));
 
 app.get('/', (request: Request, response: Response) => {
   response.send(render(request.url));
+});
+
+app.get('/galaxias', async (request: Request, response: Response) => {
+  try {
+    const { data } = await axios.get('https://images-api.nasa.gov/search?q=galaxies');
+    const initialProps = {
+      galaxies: data?.collection?.items
+    }
+    response.send(render(request.url, initialProps));
+  } catch (error) {
+    throw new Error('An error ocurred in /galaxies', error);
+  }
 });
 
 app.get('*', (request: Request, response: Response) => {
