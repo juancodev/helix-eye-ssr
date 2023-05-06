9820d92ac0ebb71f6acb890ca3a890b2fde1aff8
juancodev <montillasanchezjuancarlos@gmail.com>
2023-05-05 23:19:58 -0400

    [⚡ ADD] styles in the server

--- src/server/render/template.ts
+++ src/server/render/template.ts
@@ -1,4 +1,7 @@
-export const template = (html: string, initialProps = {}): string => {
+
+
+export const template = (html: string, initialProps = {}, style = ''): string => {
+
   return (
     `
     <!DOCTYPE html>
@@ -13,6 +16,7 @@ export const template = (html: string, initialProps = {}): string => {
       <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;600&display=swap" rel="stylesheet">
       <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
       <title>Helix App</title>
+      ${style}
     </head>
 
     <body>
